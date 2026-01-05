54cb0b4c678f51abc60bbe10f53aa36b29b172cf
4rjunc <arjunmundamani@gmail.com>
2026-01-06 01:51:34 +0530

    refactored

--- src/tui/event.rs
+++ src/tui/event.rs
@@ -1,8 +1,8 @@
+use super::{App, ResultMessage, Screen};
+use crate::commands::vote::process_fetch_vote_account;
 use crossterm::event::KeyCode;
 use solana_pubkey::Pubkey;
 use std::str::FromStr;
-
-use super::{App, ResultMessage, Screen};
 
 pub async fn handle_key(app: &mut App, key: KeyCode) {
     match &app.current_screen {
@@ -175,7 +175,7 @@ async fn execute_show_vote_account(app: &mut App) {
     app.current_screen = Screen::Loading;
 
     // Fetch vote account
-    match fetch_vote_account_info(&app.ctx, &pubkey).await {
+    match process_fetch_vote_account(&app.ctx, &pubkey).await {
         Ok(info) => {
             app.result_message = Some(ResultMessage {
                 success: true,
@@ -195,65 +195,3 @@ async fn execute_show_vote_account(app: &mut App) {
     app.clear_input();
     app.current_screen = Screen::Result;
 }
-
-async fn fetch_vote_account_info(
-    ctx: &crate::context::ScillaContext,
-    vote_account_pubkey: &Pubkey,
-) -> anyhow::Result<String> {
-    use crate::misc::helpers::lamports_to_sol;
-    use solana_vote_program::vote_state::VoteStateV4;
-
-    let vote_account = ctx
-        .rpc()
-        .get_account(vote_account_pubkey)
-        .await
-        .map_err(|_| anyhow::anyhow!("{} account does not exist", vote_account_pubkey))?;
-
-    if vote_account.owner != solana_vote_program::id() {
-        anyhow::bail!("{} is not a vote account", vote_account_pubkey);
-    }
-
-    let vote_state = VoteStateV4::deserialize(&vote_account.data, vote_account_pubkey)
-        .map_err(|_| anyhow::anyhow!("Account data could not be deserialized to vote state"))?;
-
-    let balance_sol = lamports_to_sol(vote_account.lamports);
-
-    let root_slot = match vote_state.root_slot {
-        Some(slot) => slot.to_string(),
-        None => "~".to_string(),
-    };
-
-    let timestamp = chrono::DateTime::from_timestamp(vote_state.last_timestamp.timestamp, 0)
-        .expect("Solana timestamp should always be in valid range")
-        .format("%Y-%m-%dT%H:%M:%SZ")
-        .to_string();
-
-    let vote_authority = vote_state
-        .authorized_voters
-        .last()
-        .map(|(_, v)| v.to_string())
-        .unwrap_or_else(|| vote_state.node_pubkey.to_string());
-
-    let info = format!(
-        "Balance:          {} SOL\n\
-         Validator:        {}\n\
-         Vote Authority:   {}\n\
-         Withdraw Auth:    {}\n\
-         Credits:          {}\n\
-         Commission:       {}%\n\
-         Root Slot:        {}\n\
-         Timestamp:        {} (slot {})",
-        balance_sol,
-        vote_state.node_pubkey,
-        vote_authority,
-        vote_state.authorized_withdrawer,
-        vote_state.credits(),
-        vote_state.inflation_rewards_commission_bps / 100,
-        root_slot,
-        timestamp,
-        vote_state.last_timestamp.slot
-    );
-
-    Ok(info)
-}
-
